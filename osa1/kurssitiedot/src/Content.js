0337bb353a28a93e461b6af5389eb3e177915939
Lauri Latva-Kyyny <lauri.latva-kyyny@outlook.com>
2020-07-13 21:58:27 +0300

    Add exercise 1.4.

--- osa1/kurssitiedot/src/Content.js
+++ osa1/kurssitiedot/src/Content.js
@@ -4,9 +4,9 @@ import Part from './Part';
 const Content = (props) => {
     return (
       <>
-        <Part partName={props.part1.name} exerciseAmount={props.part1.exercises}/>
-        <Part partName={props.part2.name} exerciseAmount={props.part2.exercises}/>
-        <Part partName={props.part3.name} exerciseAmount={props.part3.exercises}/>
+        {props.parts.map( (part, index) => (
+            <Part key={index} partName = {part.name} exerciseAmount = {part.exercises} />
+        ))}
       </>
     )
 }
--- osa1/kurssitiedot/src/Total.js
+++ osa1/kurssitiedot/src/Total.js
@@ -1,8 +1,15 @@
 import React from 'react'
 
+// Calculate total exercise amount from parts array
+const calculateTotalExercises = (parts) => {
+    let totalExercises = 0
+    parts.map( (part, index) => totalExercises += parts[index].exercises)
+    return totalExercises
+}
+
 const Total = (props) => {
     return (
-        <p>Number of exercises {props.total}</p>
+        <p>Number of exercises {calculateTotalExercises(props.parts)}</p>
     )
 }
 
